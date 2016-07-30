e3a1e1c548bc1a1e2a732aba656916e6cdd09a38
Kiefer Yap <kiefer.yap@gmail.com>
2016-07-30 12:34:23 +0900

    Fixed the schema and updated the SQL file

--- samurai_meetups.sql
+++ samurai_meetups.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 29, 2016 at 08:35 PM
+-- Generation Time: Jul 30, 2016 at 12:31 PM
 -- Server version: 5.5.50-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.17
 
@@ -41,9 +41,9 @@ CREATE TABLE IF NOT EXISTS `front_page_elements` (
 --
 
 CREATE TABLE IF NOT EXISTS `front_page_image_types` (
-  `front_image_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `front_page_image_type_id` int(11) NOT NULL AUTO_INCREMENT,
   `description` text,
-  PRIMARY KEY (`front_image_type_id`)
+  PRIMARY KEY (`front_page_image_type_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `front_page_image_types` (
 
 CREATE TABLE IF NOT EXISTS `languages` (
   `language_id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` text,
+  `description` varchar(16) DEFAULT NULL,
   PRIMARY KEY (`language_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -68,6 +68,7 @@ CREATE TABLE IF NOT EXISTS `reports` (
   `report_id` int(11) NOT NULL AUTO_INCREMENT,
   `sidebar_image_url` text,
   `worker_image_url` text,
+  `set_image_url` text,
   `experience_image_url` text,
   `description` text,
   PRIMARY KEY (`report_id`)
