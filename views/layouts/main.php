91862e2ceee02f36c55e5e91d4c8987a9df7b081
Kiefer Yap <kiefer.yap@gmail.com>
2016-08-03 12:40:38 +0900

    Bugfix on duplicate pages

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -66,9 +66,10 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <!-- <img class="header-logo" src="<?=Url::base()?>images/logo.png"> -->
+        <!-- <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right"><?= Yii::powered() ?></p> -->
     </div>
 </footer>
 
