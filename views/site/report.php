37f43d1032812a428d640c13145d77a64c655f05
Kiefer Yap <kiefer.yap@gmail.com>
2016-08-14 20:53:28 +0900

    UI Report Sidebar Bugfix

--- views/site/report.php
+++ views/site/report.php
@@ -7,12 +7,14 @@ use yii\helpers\Html;
 <br/>
 <div class="row reports-container">
 	<div class="col-md-3 report-sidebar">
-		<div class="col-md-12 report-sidebar-element">
-			<div class="col-md-2 report-sidebar-element-photo"></div>
-			<div class="col-md-10 report-sidebar-element-text">
-				The quick brown fox jumped over the lazy dogs. The quick brown fox jumped over the lazy dogs. The quick brown fox jumped over the lazy dogs.
+		<a href="#">
+			<div class="col-md-12 report-sidebar-element">
+				<div class="col-md-2 report-sidebar-element-photo"></div>
+				<div class="col-md-10 report-sidebar-element-text">
+					The quick brown fox jumped over the lazy dogs.
+				</div>
 			</div>
-		</div>
+		</a>
 	</div>
 	<div class="col-md-9 report-content">
 	</div>
