50e409fb2791625205f49a4b87762e24b969a45c
Awesomenite Dragonite <askawesomenitedragonite@gmail.com>
2016-09-26 11:50:55 +0000

    Git Ignore now working

--- config/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'test@example.com',
-    'facebook' => 'http://facebook.com/samuraimeetups',
-    'twitter' => 'http://twitter.com/samuraimeetups',
-    'instagram' => 'http://instagram.com/',
-    'header' => '<div id="samurai-header">SAMURAI <span id="meet-header">MEET</span>UPS</div>',
-];
