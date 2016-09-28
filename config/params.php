8a190396cd5a2ea7b5303f1efb2391299344b270
Awesomenite Dragonite <askawesomenitedragonite@gmail.com>
2016-09-28 04:00:32 +0000

    Restored some files for gitignore

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'adminEmail' => 'test@example.com',
+    'facebook' => 'http://facebook.com/samuraimeetups',
+    'twitter' => 'http://twitter.com/samuraimeetups',
+    'instagram' => 'http://instagram.com/',
+    'header' => '<div id="samurai-header">SAMURAI <span id="meet-header">MEET</span>UPS</div>',
+];
